0d176d0fb127d0186255fce4e7978f09b675a7e2
juzann09 <chesterjuz@gmail.com>
2020-07-14 19:46:32 +0400

    started using rocket classes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,65 @@
+			class Rocket{
+				constructor(name, speed, teamNumber, icon){
+					this.teamNumber = teamNumber;
+					this.speed = speed;
+					this.name = name;
+					this.icon = icon;
+				}
+				
+				choose(){
+					//document.form.rocketName="rn";
+					console.log(this);
+				}
+				launch(){}
+			}
+			
+			class RocketCurrent{
+				constructor(){
+					let rocketCurrentBlock = document.querySelector(".rocketCurrent .infoBlock");
+					this.info = rocketCurrentBlock.querySelector('ul');
+					this.head = rocketCurrentBlock.querySelector('.headline');	
+				}
+				func(){
+					alert('rocket form found!');
+				}
+			}
+
 			document.addEventListener("DOMContentLoaded", start);
 			
 			let pageChosen=1;
 			let pages = document.getElementsByClassName("page");
 			let menu = document.getElementsByClassName("menuItem");
 			
+			
+
+			
+			
+			function setRocketRadiosEvents(){
+				let rocketElements = document.querySelectorAll(".rocketCards .infoBlockContent");
+				let rocketRadios = document.getElementsByClassName("radioRocketChoice");
+				for (let i=0; i < rocketElements.length; i++){
+					console.log(i);
+				}
+				console.log('end');
+				for (let i=0; i < rocketRadios.length; i++){
+					rocketRadios[i].addEventListener('click', function(){
+						let name = rocketElements[i].querySelector('div [name="name"]').innerHTML;
+						let teamNumber = rocketElements[i].querySelector('[name="teamNumber"]').innerHTML;
+						let speed = rocketElements[i].querySelector('[name="speed"]').innerHTML;
+						let icon = rocketElements[i].querySelector('[name="icon"]').src;
+						let newRocket = new Rocket(name, speed, teamNumber, icon);
+						newRocket.choose();
+					});
+				}
+			}
+			
 			function start(){
 				setMenuEvents();
 				showPage(1);
 				activateIcon(1);
+				//let rocketCurrent1 = new rocketCurrent();
+				//rocketCurrent1.func();
+				setRocketRadiosEvents();
 			}
 			
 			function setMenuEvents(){				
