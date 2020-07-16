bc4ec3d986712a997fdafb4e36b386328ad492b7
juzann09 <chesterjuz@gmail.com>
2020-07-16 15:33:40 +0400

    team choice interaction works

--- js/script.js
+++ js/script.js
@@ -1,82 +1,290 @@
 			class Rocket{
-				constructor(){	
-				}
-				setParams(rocketElement){
-					let name = rocketElement.querySelector('[name="name"]').innerHTML;
-					let teamNumber = rocketElement.querySelector('[name="teamNumber"]').innerHTML;
-					let speed = rocketElement.querySelector('[name="speed"]').innerHTML;
-					let icon = rocketElement.querySelector('[name="icon"]').getAttribute("src");
-					
+				constructor(name,speed,teamNumber,icon){
 					this.teamNumber = teamNumber;
 					this.speed = speed;
 					this.name = name;
 					this.icon = icon;
-					//console.log(this.icon);
-					//this.writeIntoHTML();
 				}
-				writeIntoHTML(){
-					let rocketCurrentBlock = document.querySelector(".rocketCurrent");
-					//console.log(rocketCurrentBlock);
-					//this.info = rocketCurrentBlock.querySelector('ul');
-					rocketCurrentBlock.querySelector('[name="name"]').innerHTML = this.name;
-					rocketCurrentBlock.querySelector('[name="speed"]').innerHTML = this.speed;
-					rocketCurrentBlock.querySelector('[name="teamNumber"]').innerHTML = this.teamNumber;
-					rocketCurrentBlock.querySelector('[name="icon"]').setAttribute("src", this.icon);
-					//console.log("rocket built");
+				
+				stringCard(cardsBlock,i){
+					let str = `<section class="infoBlock">`;
+					str = str+ stringRocket(this);
+					str =str + `<div class="choose">
+								<input type="radio" class="radioRocketChoice" id="r${i}" name="rocketRadio">
+								<label for="r${i}">Выбрать</label>
+							</div>
+							</section>`;
+					return str;
+					
+				}
+				stringCurent(rocketCurrentBlock){
+					return rocketCurrentBlock.innerHTML=stringRocket(this);
 				}
 				launch(){
 					console.log('fly');
 				}
 			}
-
+			
+			class TeamMember{
+				constructor(name, role, icon){
+					this.name=name;
+					this.role=role;
+					this.icon=icon;
+					/* id = role-name */
+					this.id=`${this.role}-${this.name}`;
+					personID+=1;
+				}
+				
+				stringRoleBlock(role,memberIndex){
+					/* checkbox.id = role-name-index */
+					/* memberIndex - index of person in heis/her role list, 
+					each role list starts with 0-index*/
+					return `<li class="person">
+						${this.stringIcon()}
+						${this.stringName()}
+						<input type="checkbox" id="${this.id}-${memberIndex}">
+						</li>`
+				}
+				
+				stringName(){
+					return `<div>${this.name}</div>`;
+				}
+				
+				stringIcon(){
+					return `<img class="personIcon" name="icon" src="${this.icon}">`;
+				}
+			}
+			
+			/* variables */
 			document.addEventListener("DOMContentLoaded", start);
 			
-			let pageChosen;
+			let pageChosen = 2;
 			let pages;
 			let menu;
 			
+			let rockets;
 			let rocketElements;
-			let rocketElementForBuilding;
+			let rocketForBuilding;
 			let rocketRadios;
 			let rocketCurrent;
+
+			let teamChosenIcons = new Map();
+			let teamCards;
+			let teamChosen = new Set();
+			let teamAvaliable = new Map();
+			let captains = [];
+			let doctors = [];
+			let engineers = [];
+			let spaceMarines = [];
+			let teamNumberChosen = 0;
+			let teamNumberNeeded = -1;
+			let personID = 0;
+			let roles = new Map();
+			roles.set('captain','Капитан');
+			roles.set('doctor','Врач');
+			roles.set('engineer','Инженер');
+			roles.set('spaceMarine','Десантник');
 			
+			/* functions */
+			
+			/* creates avaliavle team members */
+			function setAvaliableTeamMembers(){
+				captains.push(new TeamMember("Константин1 Константинопольский", "captain", "svg/man1.svg"));
+				captains.push(new TeamMember("Константин2 Константинопольский", "captain", "svg/woman1.svg"));
+				doctors.push(new TeamMember("Константин3 Константинопольский", "doctor", "svg/man2.svg"));
+				doctors.push(new TeamMember("Константин4 Константинопольский", "doctor", "svg/woman2.svg"));
+				engineers.push(new TeamMember("Константин5 Константинопольский", "engineer", "svg/man1.svg"));
+				engineers.push(new TeamMember("Константин6 Константинопольский", "engineer", "svg/woman3.svg"));
+				spaceMarines.push(new TeamMember("Константин7 Константинопольский", "spaceMarine", "svg/man2.svg"));
+				spaceMarines.push(new TeamMember("Константин8 Константинопольский", "spaceMarine", "svg/woman1.svg"));
+				teamAvaliable.set("doctor", doctors);
+				teamAvaliable.set("captain", captains);
+				teamAvaliable.set("engineer", engineers);
+				teamAvaliable.set("spaceMarine", spaceMarines);
+			}
+			
+			/* writes team cards into html code */
+			function writeTeamCardsIntoHTML(){
+				let teamCardsBlock = document.querySelector(".teams.cards");
+				let teamCards = new Map();
+				teamCards.set("captain",teamCardsBlock.querySelector('.infoBlock.captain .infoBlockContent ul'));
+				teamCards.set("doctor",teamCardsBlock.querySelector('.infoBlock.doctor .infoBlockContent ul'));
+				teamCards.set("engineer",teamCardsBlock.querySelector('.infoBlock.engineer .infoBlockContent ul'));
+				teamCards.set("spaceMarine",teamCardsBlock.querySelector('.infoBlock.spaceMarine .infoBlockContent ul'));
+				
+				for (let roleAvaliableMembers of teamAvaliable){
+					let memberIndex = 0;
+					let role = roleAvaliableMembers[0];
+					let teamCard = teamCards.get(role);
+					// roleAvaliableMembers[1] - list of avaliable persons on this role
+					for (let member of roleAvaliableMembers[1]) {
+						teamCard.innerHTML+=member.stringRoleBlock(role,memberIndex);
+						memberIndex+=1;
+					}
+				}
+			}
+			
+			/* adds events for team choice checkboxes */
+			function setPersonChoiceEvents(){
+				let teamChosenIconsBlock = document.querySelector('.chosenRocketAndTeam .team');
+				console.log(teamChosenIconsBlock);
+				teamChosenIcons.set("captain",teamChosenIconsBlock.querySelector('.captain+ul'));
+				teamChosenIcons.set("doctor",teamChosenIconsBlock.querySelector('.doctor+ul'));
+				teamChosenIcons.set("engineer",teamChosenIconsBlock.querySelector('.engineer+ul'));
+				teamChosenIcons.set("spaceMarine",teamChosenIconsBlock.querySelector('.spaceMarine+ul'));
+				let checkboxesPersons = document.querySelectorAll('.teams.cards input[type="checkbox"]');
+				for (let checkbox1 of checkboxesPersons){
+					checkbox1.addEventListener("click", () => chooseMember(checkbox1));
+				}
+				let saveTeamButton = document.querySelector('.chosenRocketAndTeam .button.enabled');
+				saveTeamButton.addEventListener("click", () => saveTeam());
+			}
+			
+			/* deletes member from chosen team */
+			function deleteFromTeamChosen(personID){
+				for (let memChosen of teamChosen){
+					//console.log(`${personID} : ${memChosen.id}`);
+					if (personID == memChosen.id)
+						teamChosen.delete(memChosen);
+				}
+			}
+			
+			/* adds member to chosen team */
+			function chooseMember(checkbox){
+				let personID = `${checkbox.id.split('-')[0]}-${checkbox.id.split('-')[1]}`;
+				let personRole = personID.split('-')[0];
+				let personIndex = parseInt(checkbox.id.split('-')[2]);
+				if (checkbox.checked) {
+					teamNumberChosen +=1;
+					teamChosen.add(teamAvaliable.get(personRole)[personIndex]);
+					console.log('member added');
+				}
+				else  {
+					teamNumberChosen -=1;
+					deleteFromTeamChosen(personID);
+					console.log('member deleted');
+				}
+				//console.log(teamChosen);
+				console.log('Chosen team:');
+				let teamStr = getStringTeam(teamChosen);
+				console.log(teamStr);
+			}
+			
+			function clearChosenTeamIcons(){
+				for (role of teamChosenIcons.keys()){
+					teamChosenIcons.get(role).innerHTML='';
+				}
+			}
+			
+			function addMemberIntoChosenTeamBlock(member){
+				teamChosenIcons.get(member.role).innerHTML+=member.stringIcon();
+			}
+			
+			/* saves chosen team */
+			function saveTeam(){
+				if (teamNumberChosen==teamNumberNeeded){
+					console.log('team saved');
+					clearChosenTeamIcons();
+					for (let memChosen of teamChosen){
+						addMemberIntoChosenTeamBlock(memChosen);	
+					}
+				}
+				else console.log('wrong team number');
+			}
+			
+			/* returns string with info about chosen team members */
+			function getStringTeam(team){
+				let str = "";
+				//console.log(team);
+				for (let mem of team){
+					str += mem.role + ', ' + mem.name + '\n';
+				}
+				return str;
+			}
+			
+			/* returns string with html-code for rocket block */
+			function stringRocket(rocket){
+				return `<div class="rocketInfo infoBlockContent"">
+							<img class="rocketIcon" name="icon" src="${rocket.icon}">
+							<ul>
+								<li class="property" >
+									<div>Имя</div>
+									<div name="name">${rocket.name}</div>
+								</li>
+								<li class="property">
+									<div>Скорость</div>
+									<div name="speed">${rocket.speed}</div>
+								</li>
+								<li class="property">
+									<div>Экипаж</div>
+									<div name="teamNumber">${rocket.teamNumber}</div>
+								</li>	
+							</ul>
+						</div>`;
+			}
+			
+			/* adds events for rocket choice radios */
 			function setRocketEvents(){
-				rocketElements = document.querySelectorAll(".rocketCards .infoBlockContent");
-				rocketElementForBuilding = rocketElements[0];
+				rockets = [];
+				rockets.push(new Rocket("rocket1", "3 km/s", 4, "svg/alienShip.svg"));
+				rockets.push(new Rocket("rocket2", "3 km/s", 4, "svg/rocket1.svg"));
+				rockets.push(new Rocket("rocket3", "3 km/s", 4, "svg/rocket2.svg"));
+				
+				rocketCards = document.querySelector("#rocketCards");
+				//rocketCards.innerHTML = "0000000000000000000000";
+				rocketCards.innerHTML="";
+				
+				for (let i=0; i<rockets.length; i++){
+					rocketCards.innerHTML+=rockets[i].stringCard(rocketCards,i+1);
+					//console.log(rockets[i]);
+				}
+				rocketForBuilding = rockets[0];
 				rocketRadios = document.getElementsByClassName("radioRocketChoice");
-				chooseRocket(rocketElements[0]);
+				chooseRocket(rockets[0]);
 				buildRocket();
 				rocketRadios[0].checked="checked";
 				for (let i=0; i < rocketRadios.length; i++){
-					rocketRadios[i].addEventListener('click',() => chooseRocket(rocketElements[i]));
+					rocketRadios[i].addEventListener('click',() => chooseRocket(rockets[i]));
 				}
-				let rocketLaunchButton = document.querySelector(".rocketCurrent .button");
+				let rocketLaunchButton = document.querySelector("#rocketChoice .current .button");
 				rocketLaunchButton.addEventListener('click', buildRocket);
 			}
 			
+			/* creates rocket with chosen params and writes it into html */
 			function buildRocket(){
-				rocketCurrent = new Rocket();
-				rocketCurrent.setParams(rocketElementForBuilding);
-				rocketCurrent.writeIntoHTML();
+				let rocketCurrentBlock = document.querySelector("#rocketChoice .current>:nth-child(2)");
+				rocketCurrent = new Rocket(rocketForBuilding.name, rocketForBuilding.speed, rocketForBuilding.teamNumber, rocketForBuilding.icon);
+				rocketCurrentBlock.innerHTML=rocketCurrent.stringCurent(rocketCurrentBlock);
+				teamNumberNeeded = rocketCurrent.teamNumber;
 				console.log("rocket built");
 			}
 			
-			function chooseRocket(rocketElement){
-				rocketElementForBuilding = rocketElement;
-				console.log("params are ready");
+			/* sets params for rocket building */
+			function chooseRocket(rocket){
+				rocketForBuilding = rocket;
+				console.log("params for building are ready");
 			}
 			
+			/* writes current rocket into html-code of team choice page */
+			function setRocketOnTeamPage(){
+				let rocketCurrentBlockTeamPage = document.querySelector("#teamChoice .rocket.current>:nth-child(2)");
+				rocketCurrentBlockTeamPage.innerHTML=rocketCurrent.stringCurent(rocketCurrentBlockTeamPage);
+			}
 			
 			function start(){
-				pageChosen = 1;
 				pages = document.getElementsByClassName("page");
 				menu = document.getElementsByClassName("menuItem");
 				setMenuEvents();
 				showPage(pageChosen);
 				activateIcon(pageChosen);
 				setRocketEvents();
-			}
+				setAvaliableTeamMembers();
+				writeTeamCardsIntoHTML();
+				setPersonChoiceEvents();
+				setRocketOnTeamPage();
+			}			
 			
+			/* sets menu evemts */
 			function setMenuEvents(){				
 				for (let i=0; i < menu.length; i++){
 					menu[i].addEventListener('click', function(){
